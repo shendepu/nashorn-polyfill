6bc2016bd2653b93baf7c54b49dcca0108c7607e
SHEN, JIMMY <shendepu@hotmail.com>
2016-11-30 17:17:36 +0800

    Added build:prod script

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack')
+const paths = require('./paths')
+
+const webpackConfig = {
+  name    : 'client',
+  target  : 'web',
+  devtool : 'source-map',
+  resolve : {
+    modules: [
+      paths.lib(),
+      'node_modules'
+    ],
+    extensions : ['.js', '.jsx', '.json']
+  },
+  entry: paths.base('index.js'),
+  output: {
+    path: paths.build(),
+    filename: 'nashorn-polyfill.js'
+  },
+  module : {},
+  plugins: []
+}
+
+webpackConfig.plugins.push(
+  new webpack.optimize.OccurrenceOrderPlugin(),
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    compress : {
+      unused    : true,
+      dead_code : true,
+      warnings  : false
+    }
+  })
+)
+
+module.exports = webpackConfig
