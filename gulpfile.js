40e68ec963bdd9ab5a7a1bdc2cc2930d1e6fff32
SHEN, JIMMY <shendepu@hotmail.com>
2016-12-01 15:34:43 +0800

    Used gulp to run test

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gutil = require("gulp-util");
 var concat = require('gulp-concat');
 var webpack = require("webpack");
 var del = require('del');
-var paths = require('./config/paths')
+var exec = require('gulp-exec');
+var paths = require('./config/paths');
+
 
 var filename = 'nashorn-polyfill.js'
 var srcFiles = ['./lib/global-polyfill.js', './lib/timer-polyfill.js', './build/nashorn-polyfill.webpack.js']
@@ -49,3 +51,29 @@ gulp.task('scripts:dist', ['clean:dist', 'scripts:prod'], function () {
   setTimeout(copyJsToDist, 1000);
 });
 
+function addGlobalPolyfillArg(cmd) {
+  return cmd + ' ' + paths.lib('global-polyfill.js')
+}
+function addTimerPolyfillArg(cmd) {
+  return cmd + ' ' + paths.lib('timer-polyfill.js')
+}
+function addXmlHttpRequestPolyfillArg(cmd) {
+  return cmd + ' ' + paths.lib('xml-http-request-polyfill.js')
+}
+function addBlobPolyfillArg(cmd) {
+  return cmd + ' ' + paths.lib('blob-polyfill.js')
+}
+
+gulp.task('test:setTimeout', function (callback) {
+  var cmd = '/usr/bin/jjs -scripting ';
+  cmd = addGlobalPolyfillArg(cmd);
+  cmd = addTimerPolyfillArg(cmd);
+  return gulp.src('test/setTimeout.js')
+    .pipe(exec(cmd + ' <%= file.path %>', function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      callback(err);
+    }));
+});
+
+gulp.task('test', ['test:setTimeout']);
--- test/setTimeout.js
+++ test/setTimeout.js
@@ -6,5 +6,8 @@
   setTimeout(test('setTimeout 0'), 0);
   setTimeout(test('setTimeout 500ms'), 500);
 
-  print('The process has to be terminated manually afterwards, since timer-polyfill has global reference.');
+  setTimeout(function() {
+    context.__nashorn_polyfill_timer.cancel();
+    print('The process is terminated by hacking way of __nashorn_polyfill_timer.cancel().');
+  }, 2 * 1000)
 })(this);
